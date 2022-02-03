998209707bbd690d7fe90a5a07c02b3e0aac1ed2
meltsplit <seokwoo2000@naver.com>
2022-02-03 23:30:14 +0900

    Initial Commit

---
+++ GlobalWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GlobalWeatherApp
+//
+//  Created by Jr.SteveJobs on 2022/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
